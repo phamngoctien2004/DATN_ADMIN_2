9f3937bed09b722fe66718bbc330f4a4d8bbb133
tien dev <tienhvnhgpt@gmail.com>
2025-09-25 22:12:23 +0700

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "swiper/swiper-bundle.css";
+import "swiper/swiper-bundle.css"; // hoặc "swiper/css" tùy phiên bản
 import "simplebar-react/dist/simplebar.min.css";
+
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import Footer from "./components/footer/Footer.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// (tuỳ chọn) Devtools:
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Tạo 1 client dùng chung
+const queryClient = new QueryClient({
+  // tuỳ chọn khuyến nghị
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <AppWrapper>
-        <App />
-        <Footer/>
-      </AppWrapper>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AppWrapper>
+          <App />
+          <Footer />
+        </AppWrapper>
+      </ThemeProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   </StrictMode>
 );
